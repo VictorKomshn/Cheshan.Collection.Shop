67636372d0711b42739b1af58d33986b09d37677
Victor Komyshan <v.komyshan@scout-corp.com>
2023-11-25 13:00:03 +0300

    brandScript

---
+++ Cheshan.Collection.Shop/wwwroot/js/BrandScript.js
@@ -0,0 +1,20 @@
+﻿const brandPAgeparams = new URLSearchParams(window.location.search);
+const brandName = params.get('brandNames');
+
+
+function selectBrandSex(brandSex) {
+    let location = window.location.pathname;
+
+    if (!location.includes('filter')) {
+        location += '/filter';
+    }
+    if (brandSex == "man") {
+        location += "?IsMan=true";
+
+    }
+    else {
+        location += "?IsMan=false";
+    }
+
+    window.location = location;
+}
